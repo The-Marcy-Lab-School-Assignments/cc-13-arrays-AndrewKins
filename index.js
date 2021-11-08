7eeef5a3c96c8aee7e96647e762c4b4d11f412dc
Andrew Enoe <andrewm.enoe@gmail.com>
2021-11-08 15:27:04 +0000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-//write your code here
+function averageInArray(numbers){
+    let average = 0;
+    for( let i = 0; i < numbers.length; i++){
+        average += numbers[i]
+    }
+        average = average/numbers.length
+        return average
+}
+console.log(averageInArray([2,2,5,10]))
